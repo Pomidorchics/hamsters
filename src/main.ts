bbc715df6f9692f1d03094b9281696535c0228db
alevtinamuz <fghbaim@gmail.com>
2023-12-14 11:14:35 +1000

    firebase update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtOgwZ3Np3198lDmGMij5QiGIAU87bC60",
+  authDomain: "hamsters-8267d.firebaseapp.com",
+  projectId: "hamsters-8267d",
+  storageBucket: "hamsters-8267d.appspot.com",
+  messagingSenderId: "111930625032",
+  appId: "1:111930625032:web:ac52594afeb952c0e915bd",
+  measurementId: "G-WV23LVZS8Z"
+};
+
+initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 
 createApp(App).use(router).mount('#app')
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,3 +1,42 @@
 <template>
-  Register Page
+  <h1>Create an Account</h1>
+  <p><input type="email" placeholder="email" v-model="email"/></p>
+  <p><input type="password" placeholder="password" v-model="password"/></p>
+  <p><button @click="registerNewUser">Submit</button></p>
+  <!-- <p><button @click="signInWithGoogle">Sign In With Google</button></p> -->
 </template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+export default class Register extends Vue {
+    email = "";
+    password = "";
+
+    registerNewUser() {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(res => {
+            console.log(res);
+            let token = res.user.ma;
+            let userId = res.user.uid;
+
+            localStorage.setItem('token', token);
+            localStorage.setItem('userId', userId);
+
+            this.$router.push('/feed');
+            
+        }).catch(function(error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
